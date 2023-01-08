f4eb9a379e6f649d79030d3d9916d9e88cacfbad
SANAT <SANAT@DESKTOP-05KN1EP>
2023-01-08 12:38:38 +0530

    Three API's created (Get,Post and GetAll)

---
+++ springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,51 @@
+package com.springrest.springrest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.entities.Course;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+	
+	List<Course> list;
+	
+	public CourseServiceImpl()
+	{
+		list = new ArrayList<>();
+		list.add(new Course(101,"Java Course","Best Course"));
+		list.add(new Course(102,"Spring Boot","Very Usefull course"));
+	}
+
+	@Override
+	public List<Course> getCourses() {
+	
+		return list;
+	}
+
+	@Override
+	public Course getCourse(Long courseId) {
+		
+		Course c = null;
+		
+		for(Course course:list)
+		{
+			if(course.getId() == courseId)
+			{
+				c = course;
+				break;
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		
+		list.add(course);
+		return course;
+	}
+
+}
